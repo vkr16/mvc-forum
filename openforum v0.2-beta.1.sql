3d014826e4633580e89b436e0d12ebbe500dee09
Fikri Miftah <fikri.droid16@gmail.com>
2021-05-15 07:15:17 +0700

    Update database structure
    remove the "default" time for every field with time insertion field

--- openforum v0.2-beta.1.sql
+++ openforum v0.2-beta.1.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 12, 2021 at 03:03 PM
+-- Generation Time: May 15, 2021 at 02:12 AM
 -- Server version: 10.4.17-MariaDB
 -- PHP Version: 7.4.15
 
@@ -32,18 +32,8 @@ CREATE TABLE `comments` (
   `commenter` int(11) NOT NULL,
   `content` mediumtext NOT NULL,
   `post_id` int(11) NOT NULL,
-  `time_commented` datetime NOT NULL DEFAULT current_timestamp()
+  `time_commented` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `comments`
---
-
-INSERT INTO `comments` (`id`, `commenter`, `content`, `post_id`, `time_commented`) VALUES
-(1, 2, 'tes', 1, '2021-05-12 19:56:37'),
-(2, 2, 'dssddasd', 1, '2021-05-12 20:01:41'),
-(3, 2, 'sasas', 1, '2021-05-12 20:03:29'),
-(4, 2, 'saas', 1, '2021-05-12 20:03:32');
 
 -- --------------------------------------------------------
 
@@ -58,18 +48,8 @@ CREATE TABLE `notifications` (
   `sender_photo` varchar(255) NOT NULL,
   `recipient` int(11) NOT NULL,
   `post_id` int(11) NOT NULL,
-  `time` datetime NOT NULL DEFAULT current_timestamp()
+  `time_notified` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `notifications`
---
-
-INSERT INTO `notifications` (`id`, `comment_id`, `sender_username`, `sender_photo`, `recipient`, `post_id`, `time`) VALUES
-(1, 1, 'AuliaZhafran', 'user.png', 1, 1, '2021-05-12 19:56:37'),
-(2, 2, 'AuliaZhafran', 'user.png', 1, 1, '2021-05-12 20:01:41'),
-(3, 3, 'AuliaZhafran', 'user.png', 1, 1, '2021-05-12 20:03:29'),
-(4, 4, 'AuliaZhafran', 'user.png', 1, 1, '2021-05-12 20:03:32');
 
 -- --------------------------------------------------------
 
@@ -82,15 +62,8 @@ CREATE TABLE `posts` (
   `owner` int(11) NOT NULL,
   `content` mediumtext NOT NULL,
   `category` varchar(25) NOT NULL DEFAULT 'General',
-  `time_posted` datetime NOT NULL DEFAULT current_timestamp()
+  `time_posted` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `posts`
---
-
-INSERT INTO `posts` (`id`, `owner`, `content`, `category`, `time_posted`) VALUES
-(1, 1, 'sjdfgsdfs', 'General', '2021-05-12 19:56:03');
 
 -- --------------------------------------------------------
 
@@ -109,14 +82,6 @@ CREATE TABLE `users` (
   `peringkat` enum('Newbie','Helper','Ambitious','Educated','Expert','Professional','Genius','Brilliant') NOT NULL DEFAULT 'Newbie',
   `points` int(11) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `users`
---
-
-INSERT INTO `users` (`id`, `fullname`, `username`, `email`, `password`, `photo`, `register_date`, `peringkat`, `points`) VALUES
-(1, 'Fikri Miftah Akmaludin', 'FikriMiftah', 'fikrimiftah@student.ub.ac.id', '$2y$10$uBJPtiiAt3BBX21iLO5hLerzyv0pH.EmuARLyNwa.ICE6koKBESDu', 'FikriMiftah.jpg', '2021-05-12 19:55:46', 'Newbie', 0),
-(2, 'Aulia Zhafran', 'AuliaZhafran', 'zhafran@gmail.com', '$2y$10$s5t4845XO4ThjkWcwjA5r.WFWMcoUlImTtb9POUvJADDR9olXotW.', 'user.png', '2021-05-12 19:56:26', 'Newbie', 68);
 
 --
 -- Indexes for dumped tables
@@ -154,25 +119,25 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `comments`
 --
 ALTER TABLE `comments`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `notifications`
 --
 ALTER TABLE `notifications`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `posts`
 --
 ALTER TABLE `posts`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
